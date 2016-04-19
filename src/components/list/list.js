ba4423e81da41b762ddcd92e7d928042e57962b0
liutao <947809647@qq.com>
2016-04-19 10:19:36 +0800

    完善收件箱功能

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,9 +1,24 @@
+var model = {
+  set: function(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  get: function(key) {
+    return JSON.parse(localStorage.getItem(key));
+  },
+  remove: function(key) {
+    localStorage.removeItem(key);
+  },
+  clearAll: function() {
+    localStorage.clear();
+  }
+};
+
 var methods = {
   add: function() {
     this.todoList.push({
       text: this.newTodo
     });
-    console.log(this.todoList);
+    model.set('todoList', this.todoList);
   },
   complete: function($index) {
     var todoList = this.todoList,
@@ -11,19 +26,42 @@ var methods = {
     $.each(todoList, function(index, item) {
       if (+$index === index) {
         var complete = todoList.splice(index, 1)[0];
-        console.log(complete);
         completeList.push(complete);
       }
-    })
+    });
+    model.set('completeList', this.completeList);
+    model.set('todoList', this.todoList);
+  },
+  showCompleted: function() {
+    this.completeState = !this.completeState;
+    if (this.completeState) {
+      this.completedTxt = '已完成任务';
+    } else {
+      this.completedTxt = '显示已完成任务';
+    }
+  },
+  clearCompleted: function() {
+    model.remove('completeList');
+    this.completeList = [];
   }
-}
+};
+
 export default {
   data: function() {
     return {
       newTodo: '',
+      completeState: false,
+      completedTxt: '显示已完成任务',
       todoList: [],
       completeList: []
     }
   },
-  methods: methods
+  methods: methods,
+  ready: function() {
+    var todoList = model.get('todoList') || [],
+      completeList = model.get('completeList') || [];
+    // console.log(todoList, completeList);
+    this.todoList = todoList;
+    this.completeList = completeList;
+  }
 }
--- src/components/list/list.vue
+++ src/components/list/list.vue
@@ -4,19 +4,20 @@
       <i class="icon-add"></i>
       <input type="text" class="list-input" placeholder="添加任务..." v-on:keyup.enter="add" v-model="newTodo">
     </div>
-    <ul>
+    <ul class="todo-ul">
       <li class="list-item" v-for="item in todoList">
         <i class="icon-select unselect" v-on:click="complete($index)"></i>
         <div class="item-txt">{{item.text}}</div>
       </li>
     </ul>
-    <div class="completed">显示已完成任务</div>
-    <ul>
+    <div class="completed" v-on:click="showCompleted">{{completedTxt}}</div>
+    <ul class="completed-ul" v-show="completeState">
       <li class="list-item" v-for="item in completeList">
         <i class="icon-select selected" v-on:click="complete($index)"></i>
         <div class="item-txt completed-txt">{{item.text}}</div>
       </li>
     </ul>
+    <div class="completed-clear" v-on:click="clearCompleted" v-show="completeState">清空已完成任务</div>
   </div>
 </template>
 
